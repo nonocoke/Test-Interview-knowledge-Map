35484ad22937c8238eca92709144a3b4ac0c319c
wangwg <1537216090@qq.com>
2021-05-27 13:46:43 +0800

    Update 08_stack.py

--- 数据结构与算法/08_stack.py
+++ 数据结构与算法/08_stack.py
@@ -30,6 +30,10 @@ class LinkedStack:
             self.top = self.top.next
             return value
 
+    def get_top_value(self) -> int:
+        if self.top:
+            return self.top.val
+
     def __repr__(self) -> str:
         cur = self.top
         nums = []
@@ -40,8 +44,49 @@ class LinkedStack:
         return " ".join(str(num) for num in nums)
         # return " ".join(f"{num}" for num in nums)
 
+
+class NewLinkedStack(LinkedStack):
+
+    def is_empty(self):
+        return not self.top
+
+
+class Broweser():
     
-if __name__ == "__main__":
+    def __init__(self):
+        self.forward_stack = NewLinkedStack()  # 往右
+        self.back_stack = NewLinkedStack()     # 往左
+
+    def can_forward(self):
+        return False if self.forward_stack.is_empty() else True
+
+    def can_back(self):
+        return False if self.back_stack.is_empty() else True
+
+    def back(self):
+        # print("back: <--", self.back_stack)
+        if self.back_stack.is_empty():
+            return
+        _top = self.back_stack.pop()
+        self.forward_stack.push(_top)
+        # print("forward -->: ", self.forward_stack)
+        print("back to & cur in %s" % self.back_stack.get_top_value(), end="\n")
+    
+    def forward(self):
+        # print("forward -->: ", self.forward_stack)
+        if self.forward_stack.is_empty():
+            return
+        _top = self.forward_stack.pop()
+        self.back_stack.push(_top)
+        # print("back: <--", self.back_stack)
+        print("forward to  & cur in %s" % _top, end="\n")
+
+    def open(self, url):
+        print("Open new url %s" % url, end="\n")
+        self.back_stack.push(url)
+
+
+def test_linked_stack():
     stack = LinkedStack()
     for i in range(9):
         stack.push(i)
@@ -49,3 +94,24 @@ if __name__ == "__main__":
     for _ in range(3):
         stack.pop()
     print(stack)
+    
+
+def test_browser():
+    browser = Broweser()
+    browser.open('www.a')
+    browser.open('www.b')
+    browser.open('www.c')
+    if browser.can_back():
+        browser.back()
+
+    if browser.can_forward():
+        browser.forward()
+
+    browser.back()
+    browser.back()
+    browser.back()
+
+
+if __name__ == "__main__":
+    test_linked_stack()
+    test_browser()
